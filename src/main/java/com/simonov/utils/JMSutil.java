400cbce469d7526319777a87df7c9d72b72e94e7
mikhailsimonov <simonovratibor@yandex.ru>
2018-06-05 18:49:37 +0300

    add new project

---
+++ src/main/java/com/simonov/utils/JMSutil.java
@@ -0,0 +1,70 @@
+package com.simonov.utils;
+
+import com.ibm.mq.jms.JMSC;
+import com.ibm.mq.jms.MQQueueConnectionFactory;
+
+import javax.jms.*;
+
+
+public class JMSutil {
+    public static String sendMessage = null;
+    public static void putMessageToMq(String server, Integer port, String qwe, String channel, String type, String msg) {
+        try {
+            /*MQ Configuration*/
+            MQQueueConnectionFactory mqQueueConnectionFactory = new MQQueueConnectionFactory();
+            mqQueueConnectionFactory.setHostName(server);
+            mqQueueConnectionFactory.setChannel(channel);//communications link
+            mqQueueConnectionFactory.setPort(port);
+            //mqQueueConnectionFactory.setQueueManager();//service provider
+            mqQueueConnectionFactory.setTransportType(JMSC.MQJMS_TP_CLIENT_MQ_TCPIP);
+
+            /*Create Connection */
+            QueueConnection queueConnection = mqQueueConnectionFactory.createQueueConnection();
+            queueConnection.start();
+
+            /*Create session */
+            QueueSession queueSession = queueConnection.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+
+            /*Create response queue */
+            Queue queue = queueSession.createQueue(qwe);
+
+
+            /*Create text message */
+            TextMessage textMessage = queueSession.createTextMessage("put some message here");
+            textMessage.setJMSReplyTo(queue);
+            textMessage.setJMSType(type);//message type
+            textMessage.setJMSExpiration(2*1000);//message expiration
+            textMessage.setJMSDeliveryMode(DeliveryMode.PERSISTENT); //message delivery mode either persistent or non-persistemnt
+
+            /*Create sender queue */
+            QueueSender queueSender = queueSession.createSender(queueSession.createQueue(qwe));
+            queueSender.setTimeToLive(2*1000);
+            queueSender.send(textMessage);
+
+            /*After sending a message we get message id */
+            System.out.println("after sending a message we get message id "+ textMessage.getJMSMessageID());
+            sendMessage = "after sending a message we get message id "+ textMessage.getJMSMessageID();
+            String jmsCorrelationID = " JMSCorrelationID = '" + textMessage.getJMSMessageID() + "'";
+
+
+            /*Within the session we have to create queue reciver */
+            QueueReceiver queueReceiver = queueSession.createReceiver(queue,jmsCorrelationID);
+
+
+            /*Receive the message from*/
+            Message message = queueReceiver.receive(60*1000);
+            String responseMsg = ((TextMessage) message).getText();
+
+            queueSender.close();
+            queueReceiver.close();
+            queueSession.close();
+            queueConnection.close();
+
+
+        } catch (JMSException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/simonov/utils/KProducer.java
@@ -0,0 +1,45 @@
+package com.simonov.utils;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.Future;
+import java.util.logging.Logger;
+
+/**
+ * Created by Simonov-MS on 26.02.2018.
+ * Этот инструмент отправляет сообщения в очередь
+ */
+
+public class KProducer {
+
+
+    private static final Logger LOGGER = Logger.getLogger(KProducer.class.getName()); //объявляем логер для логирования
+    public static Future<RecordMetadata> kafkaSend(String url, String topic, String message){
+    //settings
+    Properties properties = new Properties();
+    properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, url);
+    properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+    properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+    //producer
+    Producer<String, String> producer = new KafkaProducer<String, String>(properties);
+    //send message
+    Future<RecordMetadata>  response =  producer.send(new ProducerRecord<String, String>(topic, message), new Callback() {
+
+        @Override
+        public void onCompletion(RecordMetadata metadata, Exception exception) {
+            LOGGER.info("check callback after send message in qwe");
+
+            if (exception != null) { //проверяю что запрос успешно отправился в очередь
+                exception.printStackTrace();
+                LOGGER.info("Exception :");
+            }else{
+                LOGGER.info("message flew into qwe successfully");
+            }
+        }
+    });
+    //end
+    producer.close();
+    return response;
+}}
